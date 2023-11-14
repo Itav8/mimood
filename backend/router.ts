4a92105232bc1673798e441f866addb1544fd7fa
Italiz <italizv@gmail.com>
2023-11-14 16:30:19 -0500

    added middleware

--- backend/router.ts
+++ backend/router.ts
@@ -1,9 +1,21 @@
 import { Router } from "express";
+import { handleInputErrors } from "./modules/middleware";
 
 const router = Router()
 
-router.get("/mood", (req, res) => {
+// Create a mood
+
+// Get all moods
+router.get("/moods", handleInputErrors, (req, res) => {
   res.json({message: "jfjf"})
 })
+
+// Get a mood by date
+// Update a mood
+
+// Create an activity
+// Get all activites
+// Get an activity by date
+// Update an activity
 
 export default router
