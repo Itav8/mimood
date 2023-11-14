eacca2611a1248e5634980f9d47861db1634158d
Italiz <italizv@gmail.com>
2023-11-13 19:27:52 -0500

    Fixed compling issue

--- backend/server.ts
+++ backend/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import router from "./router";
 import morgan from "morgan";
 import cors from "cors";
+import { protect } from "./modules/auth";
 
 const app = express();
 
@@ -15,6 +16,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.use("/api", router);
+app.use("/api", protect, router);
 
 export default app;
