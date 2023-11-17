06bdacb6b363f765aa29a582696c27fdaf5cd0b4
Italiz <italizv@gmail.com>
2023-11-16 20:22:00 -0500

    Styled landing page

--- frontend/src/pages/Landing/Landing.tsx
+++ frontend/src/pages/Landing/Landing.tsx
@@ -1,29 +1,52 @@
-import { useState } from 'react'
-import { Modal } from '../../components/Modal'
-import { Form } from '../../components/Form';
+import { useState } from "react";
+import { Modal } from "../../components/Modal";
+import { Form } from "../../components/Form";
 
-import './Landing.css'
+import "./Landing.css";
 
 export const Landing = () => {
-  const [isFormOpen, setIsFormOpen] = useState(false)
+  const [isSignUpOpen, setIsSignUpOpen] = useState(false);
+  const [isLoginOpen, setIsLoginOpen] = useState(false);
 
   return (
-    <div>
-      <h1>Mi Mood</h1>
-      <button onClick={() => {
-        setIsFormOpen(true)
-      }}>Sign Up
-      </button>
-      {isFormOpen ? (
-        <Modal
-        open={isFormOpen}
-        onClose={() => {
-          setIsFormOpen(false)
-        }}>
-          <Form title="test" />
-        </Modal>
-      ) : null}
-
+    <div className="landing-container">
+      <h1 className="landing-container_title">Mi Mood</h1>
+      <div className="button-container">
+        <button
+          onClick={() => {
+            setIsSignUpOpen(true);
+          }}
+        >
+          Sign Up
+        </button>
+        {isSignUpOpen ? (
+          <Modal
+            open={isSignUpOpen}
+            onClose={() => {
+              setIsSignUpOpen(false);
+            }}
+          >
+            <Form title="signup" />
+          </Modal>
+        ) : null}
+        <button
+          onClick={() => {
+            setIsLoginOpen(true);
+          }}
+        >
+          Login
+        </button>
+        {isLoginOpen ? (
+          <Modal
+            open={isLoginOpen}
+            onClose={() => {
+              setIsLoginOpen(false);
+            }}
+          >
+            <Form title="login" />
+          </Modal>
+        ) : null}
+      </div>
     </div>
-  )
+  );
 };
