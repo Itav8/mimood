c27fc32d627cb50b5b2bbbde17f27e18cc7aac22
Italiz <italizv@gmail.com>
2023-11-20 16:15:28 -0500

    Finsih navbar

--- frontend/src/components/Nav/Navbar.tsx
+++ frontend/src/components/Nav/Navbar.tsx
@@ -1,12 +1,17 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
+import { authRoutes } from "../../routes/routes";
 
 export const NavBar = () => {
   return (
     <div>
-      <NavLink to="/">
-        Home
-      </NavLink>
-      {}
+      <NavLink to="/">Home</NavLink>
+      {authRoutes.map((route) => {
+        return (
+          <NavLink className="navbar__link" key={route.path} to={route.path}>
+            {route.name}
+          </NavLink>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
