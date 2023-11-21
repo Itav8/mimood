e97860ac9b9d67617b1905dc6b696152149d34cc
Italiz <italizv@gmail.com>
2023-11-20 19:23:03 -0500

    Created engergy level component

---
+++ frontend/src/components/EnergyLevel/EnergyLevel.tsx
@@ -0,0 +1,8 @@
+export const EnergyLevel = () => {
+  return (
+    <div>
+      <h1>How do you feel?</h1>
+      <h4>(Tap the color best describes you)</h4>
+    </div>
+  )
+}
--- frontend/src/pages/Mood/MoodForm.tsx
+++ frontend/src/pages/Mood/MoodForm.tsx
@@ -1,7 +1,10 @@
+import { EnergyLevel } from "../../components/EnergyLevel/EnergyLevel"
+
 export const MoodForm = () => {
   return (
     <div>
       <h1>New Mood</h1>
+      <EnergyLevel />
     </div>
   )
 }
