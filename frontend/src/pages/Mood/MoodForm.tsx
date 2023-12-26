40f42c1c25664eda651f7ed060df6f5bce3af582
Italiz <italizv@gmail.com>
2023-12-26 17:49:05 -0500

    Updated the mood form flow

--- frontend/src/pages/Mood/MoodForm.tsx
+++ frontend/src/pages/Mood/MoodForm.tsx
@@ -10,9 +10,19 @@ interface MoodForm {
   energyLevel?: EnergyLevels;
 }
 
+type UserEnergyLevel = {
+  [key in EnergyLevels]: string;
+};
+
 export const MoodForm = () => {
   const navigate = useNavigate();
   const [cookies] = useCookies(["jwtToken"]);
+  const [energyLevelColors, setEnergyLevelColors] = useState<UserEnergyLevel>({
+    HIGH_ENERGY_UNPLEASANT: "#000000",
+    HIGH_ENERGY_PLEASANT: "#000000",
+    LOW_ENERGY_UNPLEASANT: "#000000",
+    LOW_ENERGY_PLEASANT: "#000000",
+  });
   const [selectedFeelings, setSelectedFeelings] = useState<
     Array<Record<string, string>>
   >([]);
@@ -44,7 +54,6 @@ export const MoodForm = () => {
 
         if (response.ok) {
           const data = await response.json();
-
           setSelectedFeelings(data.feelings[EnergyLevelsMap[energyLevel]]);
         }
       } catch (e) {
@@ -52,7 +61,36 @@ export const MoodForm = () => {
       }
     };
 
+    const fetchUserEnergyLevel = async () => {
+      const url = `${import.meta.env.VITE_API_URL}/api/userEnergyLevel`;
+      const fetchConfig: RequestInit = {
+        headers: {
+          Authorization: `Bearer ${cookies.jwtToken}`,
+          "Content-Type": "application/json",
+        },
+      };
+
+      try {
+        const response = await fetch(url, fetchConfig);
+
+        if (response.ok) {
+          const data = await response.json();
+          const userColor = data.userColor.reduce(
+            (accum: Record<string, string>, key: Record<string, string>) => {
+              accum[key.energyLevel] = key.color;
+              return accum;
+            },
+            {}
+          );
+          setEnergyLevelColors(userColor);
+        }
+      } catch (e) {
+        console.log("Error fetching userEnergyLevels list", e);
+      }
+    };
+
     fetchEnergyLevelFeelings(energyLevel);
+    fetchUserEnergyLevel();
   };
 
   const handleFormChange = (
@@ -106,34 +144,41 @@ export const MoodForm = () => {
   return (
     <div>
       <h1>New Mood</h1>
-      <EnergyLevel onClick={onEnergyLevelClick} />
+      {!selectedEnergyLevel ? (
+        <EnergyLevel onClick={onEnergyLevelClick} />
+      ) : null}
+
       <form onSubmit={handleSubmit}>
         {selectedEnergyLevel ? (
-          <div>{selectedEnergyLevel}</div>
-        ) : (
-          <div>No Energy Level Selected</div>
-        )}
-        <div>
-          <select id="feeling" name="feeling" onChange={handleFormChange}>
-            <option value="">Choose a feeling...</option>
-            {selectedFeelings.map((selectedFeeling, i) => {
-              return <option key={i}>{selectedFeeling.feelings}</option>;
-            })}
-          </select>
-        </div>
-        <div>
-          <label htmlFor="description">Description:</label>
-          <input
-            type="text"
-            id="description"
-            name="description"
-            value={form.description}
-            onChange={handleFormChange}
-          />
-        </div>
-        <div>
-          <button>Log Mood</button>
-        </div>
+          <div>
+            <div
+              style={{ backgroundColor: energyLevelColors[selectedEnergyLevel] }}
+            >
+              {selectedEnergyLevel}
+            </div>
+            <div>
+              <select id="feeling" name="feeling" onChange={handleFormChange}>
+                <option value="">Choose a feeling...</option>
+                {selectedFeelings.map((selectedFeeling, i) => {
+                  return <option key={i}>{selectedFeeling.feelings}</option>;
+                })}
+              </select>
+            </div>
+            <div>
+              <label htmlFor="description">Description:</label>
+              <input
+                type="text"
+                id="description"
+                name="description"
+                value={form.description}
+                onChange={handleFormChange}
+              />
+            </div>
+            <div>
+              <button>Log Mood</button>
+            </div>
+          </div>
+        ) : null}
       </form>
     </div>
   );
