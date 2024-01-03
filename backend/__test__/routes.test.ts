a82fa23796174b1896786fe7726a216fe1114318
Italiz <italizv@gmail.com>
2024-01-02 19:00:38 -0500

    changes

--- backend/__test__/routes.test.ts
+++ backend/__test__/routes.test.ts
@@ -1,10 +1,10 @@
-// import app from "../server";
-// import supertest from "supertest";
+import app from "../server";
+import supertest from "supertest";
 
-// describe("GET /", () => {
-//   it("Should send back data", async () => {
-//     const res = await supertest(app).get("/");
-
-//     expect(res.body.message).toBe("hello");
-//   });
-// });
+describe("GET /", () => {
+  it("Should send back data", async () => {
+    const res = await supertest(app).get("/");
+    expect(res.status).toBe(200)
+    expect(res.text).toEqual("Hello World!");
+  });
+});
--- backend/handlers/__test__/user.test.ts
+++ backend/handlers/__test__/user.test.ts
@@ -1,3 +1,51 @@
-import * as user from "../user";
+import app from "../../server";
+import request from "supertest";
+// import { createJWT } from "../../modules/auth";
+import { PrismaClient } from "@prisma/client";
+import { createNewUser } from "../user";
 
-describe("user handler", )
+const prismaMock = new PrismaClient();
+
+jest.mock("../../db");
+jest.mock("../../server");
+jest.mock("../user");
+
+beforeEach(async () => {
+  await prismaMock.$transaction([
+    prismaMock.user.deleteMany(),
+    prismaMock.userEnergyLevel.deleteMany()
+  ])
+})
+
+describe("POST /user", () => {
+  it("should create a new user", async () => {
+    const newUser = await prismaMock.user.create({
+      data: {
+        firstName: "Bob",
+        lastName: "Man",
+        color: "#802828",
+        email: "bobman@test.com",
+        password: "password",
+      },
+    });
+    const res = await request(app).post("/user").send(newUser)
+
+    expect(res.status).toBe(200);
+  });
+
+  // it("should throw an error for missing firstName", async () => {});
+  // it("should throw an error for missing lastName", async () => {});
+  // it("should throw an error for missing color", async () => {});
+  // it("should throw an error for missing email", async () => {});
+  // it("should throw an error for missing password", async () => {});
+
+  // it("should throw an error for user already exist", async () => {});
+});
+
+// describe("POST /signin", () => {
+//   it("should signin user", async () => {});
+
+//   it("should throw an error for user not found", async () => {});
+//   it("should throw an error for incorrect password", async () => {});
+//   it("should throw an error for incorrect email", async () => {});
+// });
