b030c83071ed71b7fc38d2743c00791faa0048f5
Italiz <italizv@gmail.com>
2024-01-30 18:55:33 -0500

    Modify the models

---
+++ backend/prisma/migrations/20240130235504_init/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - Made the column `description` on table `Activity` required. This step will fail if there are existing NULL values in that column.
+  - Made the column `description` on table `Mood` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "Activity" ALTER COLUMN "description" SET NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Mood" ALTER COLUMN "description" SET NOT NULL;
