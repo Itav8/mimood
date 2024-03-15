be46852b7794392b2f0fd3f674fc049a804e47e7
Italiz <italizv@gmail.com>
2024-03-15 18:26:30 -0400

    set up notification web api

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -1,29 +1,53 @@
 import { Outlet } from "react-router-dom";
 import { NavBar } from "../components/Nav/Navbar";
 import { useCookies } from "react-cookie";
-import { Flex, IconButton, useColorMode } from "@chakra-ui/react";
+import { Button, Flex, IconButton, useColorMode } from "@chakra-ui/react";
 import { MoonIcon } from "@chakra-ui/icons";
+import { useState, useEffect } from "react";
 
 export const Layout = () => {
   const [cookies] = useCookies(["jwtToken"]);
   const { toggleColorMode } = useColorMode();
+  const [notificationGranted, setNotificationGranted] = useState(false);
 
-  // const notification = new Notifation(title, {})
+  const askNotificationPermission = () => {
+    // a promise that is returned
+    // libabry built in the browser
+    Notification.requestPermission().then((permission) => {
+      if (permission === "granted") {
+        const notification = new Notification("Reminder Test", {
+          body: "You have a new message!",
+          data: { hello: "world" },
+        });
+        setNotificationGranted(true);
 
-  // const askNotificationPermission = () => {
-  //   Notification.requestPermission((permission) => {
-  //     if (permission === "granted") {
-  //       new Notification("Reminder");
-  //     }
-  //   });
-  // };
+        // notification.addEventListener("show", () => {
+        //   console.log("notification shown");
+        // })
+        console.log(notification);
+        // notification.addEventListener("close", e => {
+        //   console.log(e)
+        // })
+      }
+    });
+  };
+
+  useEffect(() => {
+    if (!notificationGranted) return;
+
+    const test = new Notification("Reminderjiji Test", {
+      body: "You have a new message!",
+      data: { hello: "world" },
+    });
+    console.log(test);
+  }, [notificationGranted]);
 
   return (
     <>
       {cookies.jwtToken ? (
         <>
           <NavBar />
-          {/* {askNotificationPermission()} */}
+          <Button onClick={() => askNotificationPermission()}>PERM</Button>
         </>
       ) : (
         <Flex justifyContent="flex-end">
