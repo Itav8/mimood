88e89bc9e198a9b3eb89698efce1676aa78d03a6
Italiz <italizv@gmail.com>
2024-03-29 23:39:25 -0400

    remove 3000

--- frontend/src/utils/getUrl.ts
+++ frontend/src/utils/getUrl.ts
@@ -1,3 +1,3 @@
 export const getApiUrl = () => {
-  return import.meta.env.VITE_API_URL || "https://dawkc68fgwyft.cloudfront.net:3000";
+  return import.meta.env.VITE_API_URL || "https://dawkc68fgwyft.cloudfront.net";
 };
