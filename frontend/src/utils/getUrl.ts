c203e2b3cefa0f5eaab7662158846194083199d9
Italiz <italizv@gmail.com>
2024-03-29 23:14:11 -0400

    updated url

--- frontend/src/utils/getUrl.ts
+++ frontend/src/utils/getUrl.ts
@@ -1,3 +1,3 @@
 export const getApiUrl = () => {
-  return import.meta.env.VITE_API_URL || "https://dawkc68fgwyft.cloudfront.net";
+  return import.meta.env.VITE_API_URL || "https://dawkc68fgwyft.cloudfront.net:3000";
 };
